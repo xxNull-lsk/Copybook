be91f8b53fbf701bf97051d51977ecd8420c3606
Allan <xxnull@163.com>
2020-06-28 23:52:11 +0800

    完成基本功能

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
+import subprocess
+import sys
+from sys import platform
+
 from reportlab.pdfgen import canvas
 from reportlab.pdfbase.ttfonts import TTFont
 from reportlab.pdfbase import pdfmetrics
 from reportlab.lib.units import cm
-
-
-# 定义注册中文字体字库的方法，此处默认为微软雅黑，
-# 字体库文件的存放路径为当前目录，所以直接使用文件名
-def set_font(canv, size, font_name='msyh', font_file='msyh.ttc'):
-    pdfmetrics.registerFont(TTFont(font_name, font_file))
-    canv.setFont(font_name, size)
+import os
+from PyQt5 import QtWidgets, QtCore
+from PyQt5.QtWidgets import QGridLayout
+import datetime
 
 
 page_width = 21
@@ -21,12 +22,33 @@ doc_height = page_height - start_y * 2
 
 col_count = 5
 col_width = doc_width / col_count
-print('doc_width', doc_width, 'col_width', col_width)
+# print('doc_width', doc_width, 'col_width', col_width)
 
 line_space = 0.6
 line_height = 1.5
+line_color = 'lightgreen'
 line_count = int((doc_height + line_space) / (line_height + line_space))
-print('line_count', line_count, (doc_height + line_space) / (line_height + line_space))
+# print('line_count', line_count, (doc_height + line_space) / (line_height + line_space))
+# col_text_colors = ['black', 'lightgrey']  # 第一行黑色，其余浅灰
+col_text_colors = ['lightgrey']  # 全部浅灰
+
+UseStdFont = False
+
+if UseStdFont:
+    font_name = 'Pinyinok'
+    font_file = 'Pinyinok.ttf'
+    font_size = 36
+    font_scan = 0.75
+else:
+    font_name = 'Pinyinok2'
+    font_file = 'Pinyinok2.ttf'
+    font_size = 28
+    font_scan = 0.67
+
+
+def set_font(canv, size):
+    pdfmetrics.registerFont(TTFont(font_name, font_file))
+    canv.setFont(font_name, size)
 
 
 def draw_4_line(canv, _x, _y):
@@ -49,7 +71,7 @@ def draw_4_line(canv, _x, _y):
 
 
 def draw_bank(canv):
-    canv.setStrokeColor('lightgreen')
+    canv.setStrokeColor(line_color)
     canv.setLineWidth(1)
     for row in range(0, line_count):
         x = start_x
@@ -57,19 +79,62 @@ def draw_bank(canv):
         draw_4_line(canv, x, y)
 
 
-def draw_text(canv, row, col, txt):
+def draw_text(canv, row, col, txt, color=None):
+    if txt == '' or txt is None:
+        return
+    if color is not None:
+        canv.setFillColor(color)
     x = start_x + col * col_width
-    y = page_height - (start_y + row * (line_height + line_space))
+    y = start_y + row * (line_height + line_space) + line_height * font_scan
     txt_width = canv.stringWidth(txt)
     x += (col_width - txt_width / cm) / 2
+    y = page_height - y  # 转换坐标系，右上角坐标系，转换成左下角
     canv.drawString(x * cm, y * cm, txt)
 
 
 def draw_mutilate_text(canv, txt):
+    curr_page = -1
     for index, t in enumerate(txt):
-        row = int(index / col_count)
+        page_index = int(index / col_count / line_count)
+        if curr_page != page_index:
+            if curr_page != -1:
+                canv.showPage()
+            # 设置字体及字号
+            set_font(canv, font_size)
+            draw_bank(canv)
+            curr_page = page_index
+        row = int(index / col_count) - line_count * curr_page
         col = int(index % col_count)
-        draw_text(canv, row, col, t)
+        if col >= len(col_text_colors):
+            color = col_text_colors[-1]
+        else:
+            color = col_text_colors[col]
+        draw_text(canv, row, col, t, color)
+
+
+def draw_text_pre_line(canv, txt, repeat=False):
+    curr_page = -1
+    line_text = []
+    for t in txt:
+        line_text.append(t)
+        for i in range(0, col_count - 1):
+            line_text.append('' if not repeat else t)
+    for index, t in enumerate(line_text):
+        page_index = int(index / col_count / line_count)
+        if curr_page != page_index:
+            if curr_page != -1:
+                canv.showPage()
+            # 设置字体及字号
+            set_font(canv, font_size)
+            draw_bank(canv)
+            curr_page = page_index
+        row = int(index / col_count) - line_count * curr_page
+        col = int(index % col_count)
+        if col >= len(col_text_colors):
+            color = col_text_colors[-1]
+        else:
+            color = col_text_colors[col]
+        draw_text(canv, row, col, t, color)
 
 
 def main():
@@ -77,48 +142,110 @@ def main():
     pdf_path = "test.pdf"
     # 建立文件
     canv = canvas.Canvas(pdf_path, pagesize=(page_width * cm, page_height * cm))
-    draw_bank(canv)
-    txt = ['zheng', 'zhuang', 'xia', 'man', 'mai', 'ni', 'wo']
+
+    txt = ['zheng', 'zhuang', 'xia', 'man', 'mai', 'ni', 'wo', 'ren', 'yu', 'rv', 'ni', 'hao', 'hai', 'di', 'ye',
+           'xie', 'liu', 'wang', 'jiao', 'bao']
+
     draw_mutilate_text(canv, txt)
-    # 完成证件PDF一页
     canv.showPage()
-    # 保存PDF文件
     canv.save()
-    return
-    # 设置字体及字号
-    set_font(canv, 16)
-    # 写入证件的相关文字信息其位置为x*cm,y*cm
-    x = 1
-    y = 28
-    xp = 8
-    yp = 20
-    info = "测试"
-    WATERMARK_TXT = "这是一个水印"
-    width = 21
-    # 15 181 90
-    canv.setStrokeColor('lightgreen')
-    canv.setLineWidth(1)
-    canv.line(x * cm, y * cm, (width - x) * cm, y * cm)
-    y += 0.5
-    canv.setDash([2, 2])
-    canv.line(x * cm, y * cm, (width - x) * cm, y * cm)
-    y += 0.5
-    canv.setDash([4, 4])
-    canv.line(x * cm, y * cm, (width - x) * cm, y * cm)
-    canv.setFillColor("lightgreen")
-    canv.drawString(x * cm, y * cm, info)
-    # 写入证件的照片信息其位置为xp*cm,yp*cm
-    canv.drawImage('./photo.jpg', xp * cm, yp * cm)
-    # 设置要添加的水印文字颜色及透明度
-    canv.setFillColorRGB(180, 180, 180, alpha=0.3)
-    # 写入水印文字，这里将水印文字放在证件照上
-    set_font(canv, 7)
-    canv.drawString(xp * cm, yp * cm + 0.5 * cm, WATERMARK_TXT)
-    # 完成证件PDF一页
-    canv.showPage()
-    # 保存PDF文件
-    canv.save()
+    if platform == 'win32':
+        os.startfile(pdf_path)
+    elif platform == 'linux':
+        subprocess.call(["xdg-open", pdf_path])
+    elif platform == 'darwin':
+        subprocess.call(["open", pdf_path])
+
+
+class MyApp:
+    def on_click_ok(self):
+        global col_text_colors, line_color
+        txt = self.edit_text.toPlainText().split()
+        pdf_path = self.edit_file_name.text()
+        canv = canvas.Canvas(pdf_path, pagesize=(page_width * cm, page_height * cm))
+
+        col_text_colors = self.combo_colors.currentData()
+        line_color = self.combo_line_color.currentData()
+
+        curr_type = self.combo_types.currentData()
+        if curr_type == 0:
+            draw_text_pre_line(canv, txt)
+        elif curr_type == 1:
+            draw_text_pre_line(canv, txt, repeat=True)
+        elif curr_type == 2:
+            draw_mutilate_text(canv, txt)
+        canv.showPage()
+        canv.save()
+        if platform == 'win32':
+            os.startfile(pdf_path)
+        elif platform == 'linux':
+            subprocess.call(["xdg-open", pdf_path])
+        elif platform == 'darwin':
+            subprocess.call(["open", pdf_path])
+
+    def __init__(self):
+        self.app = QtWidgets.QApplication(sys.argv)
+        widget = QtWidgets.QWidget()
+        widget.resize(360, 360)
+        widget.setWindowTitle("hello, pyqt5")
+        grid = QGridLayout()
+        widget.setLayout(grid)
+        grid.setSpacing(16)
+        grid.setContentsMargins(32, 16, 32, 16)
+
+        row = -1
+
+        row += 1
+        label = QtWidgets.QLabel("文件名")
+        grid.addWidget(label, row, 0)
+        self.edit_file_name = QtWidgets.QLineEdit(datetime.datetime.now().strftime('%Y-%m-%d_%H_%M_%S') + '.pdf')
+        grid.addWidget(self.edit_file_name, row, 1)
+
+        row += 1
+        label = QtWidgets.QLabel("拼音")
+        grid.addWidget(label, row, 0)
+        self.edit_text = QtWidgets.QTextEdit('a b c d e f g h i j k l m n o p q r s t u v w x y z')
+        grid.addWidget(self.edit_text, row, 1)
+
+        row += 1
+        label = QtWidgets.QLabel("线条颜色")
+        grid.addWidget(label, row, 0)
+        self.combo_line_color = QtWidgets.QComboBox()
+        self.combo_line_color.addItem('浅绿', 'lightgreen')
+        self.combo_line_color.addItem('粉色', 'lightpink')
+        self.combo_line_color.addItem('黑色', 'black')
+        grid.addWidget(self.combo_line_color, row, 1)
+
+        row += 1
+        label = QtWidgets.QLabel("文字颜色")
+        grid.addWidget(label, row, 0)
+        self.combo_colors = QtWidgets.QComboBox()
+        self.combo_colors.addItem('首行黑色，其余浅灰', ['black', 'lightgrey'])
+        self.combo_colors.addItem('全部黑色', ['black'])
+        self.combo_colors.addItem('全部浅灰', ['lightgrey'])
+        grid.addWidget(self.combo_colors, row, 1)
+
+        row += 1
+        label = QtWidgets.QLabel("样式")
+        grid.addWidget(label, row, 0)
+        self.combo_types = QtWidgets.QComboBox()
+        self.combo_types.addItem('每行仅打印首列', 0)
+        self.combo_types.addItem('每行打印一个', 1)
+        self.combo_types.addItem('常规', 2)
+        grid.addWidget(self.combo_types, row, 1)
+
+        row += 1
+        btn_ok = QtWidgets.QPushButton("确认")
+        btn_ok.clicked.connect(self.on_click_ok)
+        grid.addWidget(btn_ok, row, 1, alignment=QtCore.Qt.AlignCenter)
+
+        screen = QtWidgets.QDesktopWidget().screenGeometry()
+        size = widget.geometry()
+        widget.move((screen.width() - size.width()) / 2,
+                    (screen.height() - size.height()) / 2)
+        widget.show()
+        sys.exit(self.app.exec())
 
 
 if __name__ == '__main__':
-    main()
+    app = MyApp()
